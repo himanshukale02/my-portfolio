581aa2dbc2c86fc887d3e60d1c100a890ec671d3
Himanshu Kale <himanshudkale@gmail.com>
2025-01-02 15:52:48 +0530

    added about section and web links

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+    strings: ['&lt;Engineer&gt;', '&lt;Web Developer&gt;'],
+    typeSpeed: 65,
+});
+var url='https://www.youtube.com/'
+function openResume(){
+    window.open(url, '_blank').focus();
+}

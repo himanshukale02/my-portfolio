89a8d8e0359a375aaf72012cd91b2126e21b2110
Himanshu Kale <91282384+himanshukale02@users.noreply.github.com>
2025-04-17 19:03:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var typed = new Typed('#element', {
     strings: ['&lt;Engineer&gt;', '&lt;Web Developer&gt;'],
     typeSpeed: 65,
 });
-var url='https://drive.google.com/file/d/1fJxjJ2pMnVUR9HGqR8XhPxAntCC7O8dP/view'
+var url='https://drive.google.com/file/d/1DHtb62XNo1mZF87RGvkZxp2htOX84w-9/view?usp=sharing'
 function openResume(){
     window.open(url, '_blank').focus();
 };
